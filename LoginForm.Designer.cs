a483bd24421a62bce780eef7cab42630696bebbe
Jesper Henning Lund <jesperlund9@hotmail.com>
2023-10-02 11:01:11 +0200

    Start of project. User interface for login made

---
+++ LoginForm.Designer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Password_Manager
+{
+    public partial class LoginForm : Form
+    {
+        private TextBox usernameTextBox;
+        private TextBox passwordTextBox;
+        private Button loginButton;
+        private Label usernameLabel;
+        private Label passwordLabel;
+
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+    }
+}
---
+++ LoginForm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Password_Manager
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+        private void InitializeComponent()
+        {
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(20, 46);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(200, 22);
+            this.usernameTextBox.TabIndex = 0;
+
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(20, 131);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(200, 22);
+            this.passwordTextBox.TabIndex = 0;
+
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(70, 159);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 1;
+            this.loginButton.Text = "Login";
+
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.Location = new System.Drawing.Point(17, 20);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(100, 23);
+            this.usernameLabel.TabIndex = 1;
+            this.usernameLabel.Text = "Username";
+
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.Location = new System.Drawing.Point(17, 105);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(100, 23);
+            this.passwordLabel.TabIndex = 1;
+            this.passwordLabel.Text = "Password";
+
+            // 
+            // LoginForm
+            // 
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.passwordLabel);
+            this.Name = "LoginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            Username = usernameTextBox.Text;
+            Password = passwordTextBox.Text;
+            DialogResult = DialogResult.OK;
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
